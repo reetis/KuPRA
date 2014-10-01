e8570434459364ed34b884358768bf406c133ac6
Maksim Bezsažnyj <bezmax@gmail.com>
2014-10-01 11:15:24 +0300

    Combining PUSH MVC with PULL MVC.

---
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/PageHeader.java
@@ -0,0 +1,10 @@
+package eu.komanda30.kupra.controllers;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class PageHeader {
+    public String getTestString() {
+        return "Hello world!";
+    }
+}
