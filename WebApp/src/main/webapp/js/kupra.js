3b8c99c63fac666760033d9763a2ece9bff3246d
Rytis <rytis.stankus@gmail.com>
2014-11-26 20:25:06 +0200

    Added tooltips to image links

--- WebApp/src/main/webapp/js/kupra.js
+++ WebApp/src/main/webapp/js/kupra.js
@@ -4,10 +4,14 @@ $j(function() {
     $j(".linkImage").hover(function() {
         $j(this).animate({
             opacity: 0.5
-        });
+        }, 100);
     }, function() {
         $j(this).stop(true, true).animate({
             opacity: 1
-        });
+        }, 100);
     });
+});
+
+$j(document).ready(function() {
+    $j("body").tooltip({ selector: '[data-toggle=tooltip]' });
 });
