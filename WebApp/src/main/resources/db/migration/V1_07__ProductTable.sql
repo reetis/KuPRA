d60efd67ab916c656d1cf574831e5d49a172b921
Ignas Bobinas <ignas.bobinas93@gmail.com>
2014-11-27 23:57:02 +0200

    Product Table SQL Syntax fix

--- WebApp/src/main/resources/db/migration/V1_07__ProductTable.sql
+++ WebApp/src/main/resources/db/migration/V1_07__ProductTable.sql
@@ -2,6 +2,6 @@ CREATE TABLE product (
   id            int PRIMARY KEY,
   name                VARCHAR(64) NOT NULL,
   selected_unit        int,
-  description         VARCHAR(256),
+  description         VARCHAR(256)
 );
 CREATE SEQUENCE product_seq;
