398057439a51d48b72a01a1af9ce98d8db85c09f
Ignas Bobinas <ignas.bobinas93@gmail.com>
2014-12-03 20:07:00 +0200

    Dev-postgres Properties fix for photo upload + Few Recipes For Test Data

---
+++ WebApp/src/main/resources/db/migration-testdata/V1_13__RecipeData.sql
@@ -0,0 +1,8 @@
+INSERT INTO public.recipe (recipe_id, author, name, cooking_time, servings, public_access, description, process_description) VALUES (1, 'Hudas', 'Pirmasis Receptas', 150, 4, true, 'Puikus receptukas', '1. Paimti produktus
+2. Pagaminti
+3. Suvalgyti');
+INSERT INTO public.recipe (recipe_id, author, name, cooking_time, servings, public_access, description, process_description) VALUES (2, 'Hudas', 'Kietas Receptas', 45, 2, false, 'Receptas gaminamas dažniausiai iš pieno ir Bananaų, labai skanu prie žuvies', '1. Nulupam banaus
+2. Nuskutam žuvį
+3. Suplakam
+4. Pakepam');
+INSERT INTO public.recipe (recipe_id, author, name, cooking_time, servings, public_access, description, process_description) VALUES (3, 'lukas_babelis', 'Kebabas', 120, 2, false, 'Pukus Kebabas', '1. Apdorojam');
