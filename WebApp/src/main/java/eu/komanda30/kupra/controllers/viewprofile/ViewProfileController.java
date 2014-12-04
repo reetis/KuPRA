d6e2f9dc1491342007feab9b665d73e1a6d1610f
Rytis <rytis.stankus@gmail.com>
2014-12-04 21:46:25 +0200

    Added photo to profile view

--- WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/ViewProfileController.java
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/ViewProfileController.java
@@ -1,11 +1,14 @@
 package eu.komanda30.kupra.controllers.viewprofile;
 
+import eu.komanda30.kupra.controllers.editprofile.ProfilePhotoList;
 import eu.komanda30.kupra.entity.KupraUser;
 import eu.komanda30.kupra.entity.Recipe;
 import eu.komanda30.kupra.entity.UserProfile;
+import eu.komanda30.kupra.entity.UserProfileImage;
 import eu.komanda30.kupra.repositories.Friendships;
 import eu.komanda30.kupra.repositories.KupraUsers;
 import eu.komanda30.kupra.repositories.Recipes;
+import eu.komanda30.kupra.uploads.UploadedImageInfo;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -15,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 /**
  * Created by Rytis on 2014-12-02.
@@ -33,34 +37,36 @@ public class ViewProfileController {
     @Resource
     private Recipes recipes;
 
-    @RequestMapping(value = " /{userId}", method= RequestMethod.GET)
-    public String showRecipes(@PathVariable String userId, final ProfileInfo profileInfo) {
+    @Transactional
+    @RequestMapping(value = " /{userId}", method = RequestMethod.GET)
+    public String showRecipes(@PathVariable String userId, final ProfileInfo profileInfo, final ProfilePhotoList profilePhotoList) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
         final KupraUser user = kupraUsers.findOne(auth.getName());
-        final KupraUser friend = kupraUsers.findOne(userId);
-        final UserProfile friendUserProfile = friend.getUserProfile();
-        boolean showEverything = friendships.isFriends(user, friend) ||  userId.contentEquals(auth.getName());
+        final KupraUser targetUser = kupraUsers.findOne(userId);
+        final UserProfile friendUserProfile = targetUser.getUserProfile();
+        boolean showEverything = friendships.isFriends(user, targetUser) || userId.contentEquals(auth.getName());
         Iterable<Recipe> allRecipes;
 
         profileInfo.setName(friendUserProfile.getName());
         profileInfo.setSurname(friendUserProfile.getSurname());
         profileInfo.setEmail(friendUserProfile.getEmail());
         profileInfo.setDescription(friendUserProfile.getDescription());
-        profileInfo.setFriend(friendships.isFriends(user, friend));
+        profileInfo.setFriend(friendships.isFriends(user, targetUser));
 
-//        final UserProfileImage mainPhoto = friendUserProfile.getMainPhoto();
-//        if (mainPhoto != null) {
-//            profileInfo.setPhoto(new UploadedImageInfo(MAIN_PHOTO_REPO_ID,
-//                    mainPhoto.getImageUrl(), mainPhoto.getThumbUrl()));
-//        }
-
-        if (showEverything) {
-            allRecipes = recipes.findByUser(friend, new PageRequest(0, 100)); //TODO: pataisyti
-        } else {
-            allRecipes = recipes.findByUserPublic(friend, new PageRequest(0, 100)); //TODO: pataisyti
+        final UserProfileImage mainPhoto = targetUser.getUserProfile().getMainPhoto();
+        if (mainPhoto != null) {
+            profilePhotoList.setMainPhoto(
+                    new UploadedImageInfo(MAIN_PHOTO_REPO_ID,
+                            mainPhoto.getImageUrl(), mainPhoto.getThumbUrl()));
         }
 
-        for (Recipe r: allRecipes) {
+        if (showEverything) {
+            allRecipes = recipes.findByUser(targetUser, new PageRequest(0, 100)); //TODO: pataisyti
+        } else {
+            allRecipes = recipes.findByUserPublic(targetUser, new PageRequest(0, 100)); //TODO: pataisyti
+        }
+
+        for (Recipe r : allRecipes) {
             RecipePreview recipePreview = new RecipePreview();
             recipePreview.setName(r.getName());
             recipePreview.setDescription(r.getDescription());
