4b22443db554d5409596e6ec1f177448abd3cc6e
gintre <gintarestalygaite@gmail.com>
2014-12-07 15:34:28 +0200

    pataisiau

--- WebApp/src/main/java/eu/komanda30/kupra/controllers/menu/MenuController.java
+++
@@ -1,7 +0,0 @@
-package eu.komanda30.kupra.controllers.menu;
-
-/**
- * Created by Gintare on 2014-12-07.
- */
-public class MenuController {
-}
--- WebApp/src/main/resources/db/migration/V1_13__MenuTable.sql
+++
@@ -1,7 +0,0 @@
-CREATE SEQUENCE menu_seq START WITH 1;
-CREATE TABLE menu (
-  id INT PRIMARY KEY DEFAULT nextval('menu_seq'),
-  recipe_id  INT NOT NULL REFERENCES recipe (recipe_id),
-  menu_date DATE,
-  menu_time TIMESTAMP,
-);
