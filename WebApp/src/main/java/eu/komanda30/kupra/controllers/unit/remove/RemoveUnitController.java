983d8fe3b8c723675a9e063736fc6894bd768fb2
Ignas Bobinas <ignas.bobinas93@gmail.com>
2014-12-07 19:03:58 +0200

    Unit Delete

---
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/unit/remove/RemoveUnitController.java
@@ -0,0 +1,41 @@
+package eu.komanda30.kupra.controllers.unit.remove;
+
+import eu.komanda30.kupra.entity.Unit;
+import eu.komanda30.kupra.repositories.Products;
+import eu.komanda30.kupra.repositories.Units;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.annotation.Resource;
+
+/**
+ * Created by Ignas on 12/7/2014.
+ */
+@Controller
+@RequestMapping("/unit")
+public class RemoveUnitController {
+
+    @Resource
+    private Products products;
+
+    @Resource
+    private Units units;
+
+    @ResponseBody
+    @RequestMapping(value="/delete", method = RequestMethod.POST)
+    public String deleteUnit(@RequestParam("unit_id") Integer unitId) {
+        Unit unit = units.findOne(unitId);
+        if (unit != null){
+            if (products.isUsedUnit(unit)){
+                return "false";
+            }else{
+                units.delete(unit);
+            }
+        }
+
+        return "";
+    }
+}
