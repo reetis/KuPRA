d03533a04360700381fc131208d1ad551e202e56
Maksim Bezsažnyj <bezmax@gmail.com>
2014-12-07 11:24:07 +0200

    Added jetty session persistance.

--- WebApp/src/main/resources/db/migration/V1_02__Units.sql
+++ WebApp/src/main/resources/db/migration/V1_02__Units.sql
@@ -2,5 +2,5 @@ CREATE SEQUENCE unit_seq START WITH 1;
 CREATE TABLE unit (
   id INT PRIMARY KEY DEFAULT nextval('unit_seq'),
   name         VARCHAR(64) NOT NULL,
-  abbreviation VARCHAR(8)  NOT NULL
+  abbreviation VARCHAR(12) NOT NULL
 );
--- WebApp/src/main/resources/db/migration/V1_05__ProductTable.sql
+++ WebApp/src/main/resources/db/migration/V1_05__ProductTable.sql
@@ -3,5 +3,5 @@ CREATE TABLE product (
   id INT PRIMARY KEY DEFAULT nextval('product_seq'),
   name        VARCHAR(64) NOT NULL,
   unit        INT REFERENCES unit (id),
-  description VARCHAR(256)
+  description TEXT
 );
