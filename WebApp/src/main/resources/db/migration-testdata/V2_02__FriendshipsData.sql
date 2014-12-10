b7988c3a835781a1d4ed2607beb0fcb90a2786ec
Rytis <rytis.stankus@gmail.com>
2014-12-10 19:34:46 +0200

    Added back testing friendships

---
+++ WebApp/src/main/resources/db/migration-testdata/V2_02__FriendshipsData.sql
@@ -0,0 +1,26 @@
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('Hudas', 'rytis_stankus', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('rytis_stankus', 'Hudas', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('Hudas', 'lukas_babelis', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('lukas_babelis', 'Hudas', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('Hudas', 'gintare_stalygaite', FALSE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('rytis_stankus', 'lukas_babelis', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('lukas_babelis', 'rytis_stankus', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('gintare_stalygaite', 'lukas_babelis', TRUE);
+
+INSERT INTO friendship (target_id, source_id, friendship_status)
+VALUES ('lukas_babelis', 'gintare_stalygaite', TRUE);
