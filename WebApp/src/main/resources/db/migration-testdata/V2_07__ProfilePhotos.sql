20795ed5eaea957203889ae61d2042c586e106f9
Lukas <babelis.lukas@gmail.com>
2014-12-10 17:38:43 +0200

    profile photos added

---
+++ WebApp/src/main/resources/db/migration-testdata/V2_07__ProfilePhotos.sql
@@ -0,0 +1,10 @@
+INSERT INTO public.user_profile_image (user_id, image_url, thumb_url)
+VALUES ('lukas_babelis', '/test-uploads/luko_prof.jpg', '/test-uploads/luko_prof_thumb.jpg');
+INSERT INTO public.user_profile_image (user_id, image_url, thumb_url)
+VALUES ('admin', '/test-uploads/admin_prof.jpg', '/test-uploads/admin_prof_thumb.jpg');
+INSERT INTO public.user_profile_image (user_id, image_url, thumb_url)
+VALUES ('Hudas', '/test-uploads/ignas_prof.jpg', '/test-uploads/ignas_prof_thumb.jpg');
+INSERT INTO public.user_profile_image (user_id, image_url, thumb_url)
+VALUES ('rytis_stankus', '/test-uploads/rytis_prof.jpg', '/test-uploads/rytis_prof_thumb.jpg');
+INSERT INTO public.user_profile_image (user_id, image_url, thumb_url)
+VALUES ('gintare_stalygaite', '/test-uploads/gintare_prof.jpg', '/test-uploads/gintare_prof_thumb.jpg');
