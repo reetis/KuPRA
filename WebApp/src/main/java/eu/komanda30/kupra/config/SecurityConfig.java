40e9412bffc624f0768216cc3255c802aa616883
Maksim Bezsažnyj <bezmax@gmail.com>
2014-12-11 20:34:50 +0200

    Enabled spring security annotation config.

--- WebApp/src/main/java/eu/komanda30/kupra/config/SecurityConfig.java
+++ WebApp/src/main/java/eu/komanda30/kupra/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package eu.komanda30.kupra.config;
 
+import javax.annotation.Resource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
@@ -7,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -14,10 +17,9 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
-import javax.annotation.Resource;
-
 @Configuration
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
 @ComponentScan({ "eu.komanda30.kupra.security" })
 @PropertySource("classpath:/security.properties")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
--- WebApp/src/main/java/eu/komanda30/kupra/controllers/unit/remove/RemoveUnitController.java
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/unit/remove/RemoveUnitController.java
@@ -3,13 +3,15 @@ package eu.komanda30.kupra.controllers.unit.remove;
 import eu.komanda30.kupra.entity.Unit;
 import eu.komanda30.kupra.repositories.Products;
 import eu.komanda30.kupra.repositories.Units;
+
+import javax.annotation.Resource;
+
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-
-import javax.annotation.Resource;
 
 /**
  * Created by Ignas on 12/7/2014.
@@ -24,6 +26,7 @@ public class RemoveUnitController {
     @Resource
     private Units units;
 
+    @Secured("ADMIN")
     @ResponseBody
     @RequestMapping(value="/delete", method = RequestMethod.POST)
     public String deleteUnit(@RequestParam("unit_id") Integer unitId) {
