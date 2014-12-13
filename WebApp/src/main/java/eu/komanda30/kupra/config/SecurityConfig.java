2faf18159af3b79d7ef352904a5073c5436c61a2
Maksim Bezsažnyj <bezmax@gmail.com>
2014-12-13 16:29:38 +0200

    Fixed sql. Added password reset link to login page.

--- WebApp/src/main/java/eu/komanda30/kupra/config/SecurityConfig.java
+++ WebApp/src/main/java/eu/komanda30/kupra/config/SecurityConfig.java
@@ -63,6 +63,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                         "/login*",
                         "/error/**",
                         "/registration/**",
+                        "/resetPassword/**",
                         "/css/**",
                         "/fonts/**",
                         "/images/**",
--- WebApp/src/main/resources/db/migration/V1_15__ResetPassword.sql
+++ WebApp/src/main/resources/db/migration/V1_15__ResetPassword.sql
@@ -1,5 +1,4 @@
-ALTER TABLE password_auth
-ADD COLUMN reset_password_token VARCHAR(64) NULL,
-ADD COLUMN reset_password_token_valid_till TIMESTAMP NULL;
+ALTER TABLE password_auth ADD COLUMN reset_password_token VARCHAR(64) NULL;
+ALTER TABLE password_auth ADD COLUMN reset_password_token_valid_till TIMESTAMP NULL;
 
-CREATE INDEX ON password_auth (reset_password_token, reset_password_token_valid_till);
+CREATE INDEX password_auth_reset_token ON password_auth (reset_password_token, reset_password_token_valid_till);
