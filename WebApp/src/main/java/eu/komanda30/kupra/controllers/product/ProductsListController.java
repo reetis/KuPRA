9d3bb5793721debdc81f17b33ab0371b82e7d71e
Maksim Bezsažnyj <bezmax@gmail.com>
2014-12-14 14:25:56 +0200

    Fixes in product list controller.

--- WebApp/src/main/java/eu/komanda30/kupra/controllers/product/ProductsListController.java
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/product/ProductsListController.java
@@ -2,47 +2,59 @@ package eu.komanda30.kupra.controllers.product;
 
 import eu.komanda30.kupra.entity.Product;
 import eu.komanda30.kupra.repositories.Products;
-import eu.komanda30.kupra.repositories.Recipes;
-import eu.komanda30.kupra.repositories.Units;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.StreamSupport;
 
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@RequestMapping("/product-list")
+@RequestMapping("/products")
 public class ProductsListController {
-
     @Resource
     private Products products;
 
-    @Resource
-    private Recipes recipes;
-
-    @Resource
-    private Units units;
-
     @RequestMapping(method = RequestMethod.GET)
-    public String showProducts(final ProductsListForm list) {
-        Iterable<Product> allProducts = products.findAll();
-        for(Product p : allProducts){
-            final ProductItem fridgesItem = new ProductItem();
-            fridgesItem.setIsUsed(products.isUsedInFridge(p) || products.isUsedInRecipes(p));
-            fridgesItem.setProductId(p.getId());
-            fridgesItem.setName(p.getName());
-            fridgesItem.setUnit(p.getUnit().getAbbreviation());
-            fridgesItem.setDescription(p.getDescription());
-            list.addItem(fridgesItem);
-        }
+    public String showProducts() {
         return "product-list";
     }
 
-//    @ModelAttribute("products")
-//    public Iterable<Product> getUnits() {
-//        return this.
-//    }
+    @ResponseBody
+    @RequestMapping(value="/{product_id}", method = RequestMethod.DELETE)
+    public String deleteUnit(@PathVariable("product_id") int productId) {
+        final Product product = products.findOne(productId);
+        if (product != null){
+            if (products.isUsedInRecipes(product) || products.isUsedInFridge(product)){
+                return "false";
+            }else{
+                products.delete(product);
+            }
+        }
+
+        return "";
+    }
+
+    @ModelAttribute("products")
+    public List<ProductItem> getAllProducts() {
+        return StreamSupport.stream(products.findAll().spliterator(), false)
+                .map(p -> {
+                    final ProductItem fridgesItem = new ProductItem();
+                    fridgesItem.setIsUsed(products.isUsedInFridge(p) || products.isUsedInRecipes(p));
+                    fridgesItem.setProductId(p.getId());
+                    fridgesItem.setName(p.getName());
+                    fridgesItem.setUnit(p.getUnit().getAbbreviation());
+                    fridgesItem.setDescription(p.getDescription());
+                    return fridgesItem;
+                }).collect(Collectors.toList());
+    }
 }
 
 
