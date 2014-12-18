a8f74db180bb0abb1b9e6ab626de790d069ea99b
Rytis <rytis.stankus@gmail.com>
2014-12-18 22:50:34 +0200

    Somewhat fixed profile view layout

--- WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/RecipePreview.java
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/RecipePreview.java
@@ -1,11 +1,19 @@
 package eu.komanda30.kupra.controllers.viewprofile;
 
+import eu.komanda30.kupra.entity.RecipeImage;
+
 public class RecipePreview {
     private String name;
 
     private boolean publicAccess;
 
+    private boolean accessible;
+
     private String description;
+
+    private int recipeId;
+
+    private RecipeImage recipeImage;
 
     public String getName() {
         return name;
@@ -29,5 +37,29 @@ public class RecipePreview {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public boolean isAccessible() {
+        return accessible;
+    }
+
+    public void setAccessible(boolean accessible) {
+        this.accessible = accessible;
+    }
+
+    public int getRecipeId() {
+        return recipeId;
+    }
+
+    public void setRecipeId(int recipeId) {
+        this.recipeId = recipeId;
+    }
+
+    public RecipeImage getRecipeImage() {
+        return recipeImage;
+    }
+
+    public void setRecipeImage(RecipeImage recipeImage) {
+        this.recipeImage = recipeImage;
     }
 }
--- WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/ViewProfileController.java
+++ WebApp/src/main/java/eu/komanda30/kupra/controllers/viewprofile/ViewProfileController.java
@@ -7,10 +7,6 @@ import eu.komanda30.kupra.entity.UserProfile;
 import eu.komanda30.kupra.repositories.Friendships;
 import eu.komanda30.kupra.repositories.KupraUsers;
 import eu.komanda30.kupra.repositories.Recipes;
-
-import javax.annotation.Resource;
-import javax.transaction.Transactional;
-
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.core.Authentication;
@@ -20,6 +16,9 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.annotation.Resource;
+import javax.transaction.Transactional;
 
 @Controller
 @RequestMapping("/user")
@@ -69,6 +68,13 @@ public class ViewProfileController {
             recipePreview.setName(r.getName());
             recipePreview.setDescription(r.getDescription());
             recipePreview.setPublicAccess(r.isPublicAccess());
+            recipePreview.setAccessible(user == r.getAuthor());
+            recipePreview.setRecipeId(r.getRecipeId());
+
+            if (!r.getRecipeImages().isEmpty()) {
+                recipePreview.setRecipeImage(r.getRecipeImages().get(0));
+            }
+
             profileInfo.addRecipe(recipePreview);
         }
 
